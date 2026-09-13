324af79e3db558dc7f599c9db6e9e237b0d87c76
MiaoMing Chen <miaomingc@stumail.nwu.edu.cn>
2026-09-14 00:14:24 +0800

    bridge: define public-to-loopback bridge API

---
+++ src/bridge/bridge.h
@@ -0,0 +1,37 @@
+#ifndef TCP_SHIFT_BRIDGE_BRIDGE_H
+#define TCP_SHIFT_BRIDGE_BRIDGE_H
+
+#include <stdint.h>
+
+#include "lwip/tcp.h"
+#include "runtime/lwip_loop.h"
+
+struct tcp_shift_bridge_flow;
+
+struct tcp_shift_bridge {
+    struct tcp_shift_lwip_loop *loop;
+    struct tcp_pcb *listener;
+    struct tcp_shift_bridge_flow *flows;
+    uint16_t public_port;
+    uint16_t backend_port;
+    uint64_t accepts;
+    uint64_t backend_connects;
+    uint64_t public_to_backend_bytes;
+    uint64_t backend_to_public_bytes;
+    uint64_t active_flows;
+    uint64_t peak_active_flows;
+    uint64_t backend_failures;
+    uint64_t public_errors;
+};
+
+int tcp_shift_bridge_start_ipv4(struct tcp_shift_bridge *bridge,
+                                struct tcp_shift_lwip_loop *loop,
+                                uint16_t public_port,
+                                uint16_t backend_port);
+int tcp_shift_bridge_start_ipv6(struct tcp_shift_bridge *bridge,
+                                struct tcp_shift_lwip_loop *loop,
+                                uint16_t public_port,
+                                uint16_t backend_port);
+void tcp_shift_bridge_stop(struct tcp_shift_bridge *bridge);
+
+#endif /* TCP_SHIFT_BRIDGE_BRIDGE_H */
