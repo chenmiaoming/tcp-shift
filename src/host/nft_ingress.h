a3dc32bc69c2f3e90bc21e23ce368bff4d8b66b2
MiaoMing Chen <miaomingc@stumail.nwu.edu.cn>
2026-09-13 23:14:13 +0800

    Make nft ingress family-aware

--- src/host/nft_ingress.h
+++ src/host/nft_ingress.h
@@ -7,16 +7,24 @@
 
 struct tcp_shift_nft_ingress {
     char table_name[TCP_SHIFT_NFT_TABLE_NAME_MAX];
+    unsigned ip_version;
     int installed;
 };
 
 int tcp_shift_host_ipv4_forwarding_enabled(void);
+int tcp_shift_host_ipv6_forwarding_enabled(void);
 
 int tcp_shift_nft_ingress_install_ipv4(struct tcp_shift_nft_ingress *ingress,
                                        const char *table_name,
                                        const char *public_ipv4,
                                        uint16_t public_port,
                                        const char *target_ipv4,
+                                       uint16_t target_port);
+int tcp_shift_nft_ingress_install_ipv6(struct tcp_shift_nft_ingress *ingress,
+                                       const char *table_name,
+                                       const char *public_ipv6,
+                                       uint16_t public_port,
+                                       const char *target_ipv6,
                                        uint16_t target_port);
 
 int tcp_shift_nft_ingress_remove(struct tcp_shift_nft_ingress *ingress);
