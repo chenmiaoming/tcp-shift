1ed52cbccf527f8182c911fcf447879bb2b1a788
MiaoMing Chen <miaomingc@stumail.nwu.edu.cn>
2026-09-14 14:07:44 +0800

    Add lwIP congestion-control adapter interface

---
+++ src/lwip/cc_adapter.h
@@ -0,0 +1,35 @@
+#ifndef TCP_SHIFT_LWIP_CC_ADAPTER_H
+#define TCP_SHIFT_LWIP_CC_ADAPTER_H
+
+#include <stdint.h>
+
+#include "cc/cc.h"
+#include "cc/reno.h"
+#include "lwip/cc_hooks.h"
+#include "lwip/tcp.h"
+
+struct tcp_shift_lwip_cc_stats {
+    uint64_t bindings;
+    uint64_t ack_events;
+    uint64_t loss_events;
+    uint64_t timeout_events;
+    uint64_t policy_updates;
+    uint32_t last_cwnd_bytes;
+    uint32_t last_ssthresh_bytes;
+};
+
+struct tcp_shift_lwip_cc_adapter {
+    struct tcp_shift_lwip_cc_hook hook;
+    struct tcp_shift_cc controller;
+    struct tcp_shift_reno_state reno;
+    struct tcp_shift_lwip_cc_stats *stats;
+    struct tcp_pcb *pcb;
+    unsigned bound;
+};
+
+int tcp_shift_lwip_cc_adapter_bind(struct tcp_shift_lwip_cc_adapter *adapter,
+                                   struct tcp_pcb *pcb,
+                                   struct tcp_shift_lwip_cc_stats *stats);
+void tcp_shift_lwip_cc_adapter_unbind(struct tcp_shift_lwip_cc_adapter *adapter);
+
+#endif /* TCP_SHIFT_LWIP_CC_ADAPTER_H */
